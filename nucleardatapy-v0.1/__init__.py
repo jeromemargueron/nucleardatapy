8c682058007b23703980a427d639d24f06ffa4b6
Sudhanva Lalit <sudsid@gmail.com>
2024-11-18 11:17:09 -0500

    updated git ignore

--- nucleardatapy-v0.1/__init__.py
+++ nucleardatapy-v0.1/__init__.py
@@ -1,25 +1,33 @@
 """
 This module provides microscopic, phenomenological and experimental data constraints.
 """
-from nucleardatapy.cst              import *
-from nucleardatapy.env              import *
-from nucleardatapy.param            import *
+
+from nucleardatapy.cst import *
+from nucleardatapy.env import *
+from nucleardatapy.param import *
+
 #
-from nucleardatapy.setup_ffg          import *
-from nucleardatapy.setup_micro_matter import *
+from nucleardatapy.setup_ffg import *
+
+# from nucleardatapy.setup_micro_matter import *
 from nucleardatapy.setup_micro_matter_band import *
-from nucleardatapy.setup_micro_LP     import *
+from nucleardatapy.setup_micro_LP import *
 from nucleardatapy.setup_pheno_matter import *
-from nucleardatapy.setup_hic_matter   import *
+from nucleardatapy.setup_hic_matter import *
+
 #
-from nucleardatapy.setup_EsymLsym   import *
-from nucleardatapy.setup_EsymDen    import *
+from nucleardatapy.setup_EsymLsym import *
+from nucleardatapy.setup_EsymDen import *
+
 #
-from nucleardatapy.setup_ISGMR      import *
+from nucleardatapy.setup_ISGMR import *
+
 #
-from nucleardatapy.setup_rad_ch     import *
+from nucleardatapy.setup_rad_ch import *
+
 #
 from nucleardatapy.setup_masses_exp import *
 from nucleardatapy.setup_masses_theory import *
+
 #
-from nucleardatapy.setup_crust      import *
+from nucleardatapy.setup_crust import *
