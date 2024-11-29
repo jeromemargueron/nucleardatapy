47241219e9f1cc728887cf624e0bf8442d544411
mariana-dutra <62902743+mariana-dutra@users.noreply.github.com>
2024-11-29 16:40:32 -0300

    create sample_SetupNeutronSkin.py

---
+++ version1.0/nucleardatapy_samples/samples/sample_SetupNeutronSkin.py
@@ -0,0 +1,43 @@
+
+import os
+import sys
+import numpy as np
+
+nucleardatapy_tk = os.getenv('NUCLEARDATAPY_TK')
+sys.path.insert(0, nucleardatapy_tk)
+
+import nucleardatapy as nuda
+
+def main():
+    #
+    print(50*'-')
+    print("Enter sample_SetupNucNskin.py:")
+    print(50*'-')
+    #
+    sources, sources_lower = nuda.nuc_nskin( )[0]
+    print('Complete list of available sources:', sources )
+    #
+    sources = ['48Ca', '208Pb']
+    #
+    print('sources considered:',sources)
+    #
+    for source in sources:
+        #
+        # get the neutron skin associated to `source` and `cal`
+        #
+        # 
+        cals = nuda.nuc_nskin_source( source = source )
+        print(f'source: {source}, cals: {cals}')
+        #
+        for cal in cals:
+            nskin = nuda.SetupNeutronSkin( source = source, cal = cal )
+            nskin.print_output( )
+            #
+    #
+    print(50*'-')
+    print("Exit sample_SetupNucNskin.py:")
+    print(50*'-')
+    #
+    
+if __name__ == "__main__":
+    main()
