136c91d661bef4641f6c5c3bcd7622c3d4df9a52
mariana-dutra <62902743+mariana-dutra@users.noreply.github.com>
2024-12-10 15:37:16 -0300

    update

--- version1.0/nucleardatapy/nuc/__init__.py
+++ version1.0/nucleardatapy/nuc/__init__.py
@@ -1,17 +1,19 @@
 """
 This module provides microscopic, phenomenological and experimental data constraints.
 """
-from nucleardatapy.cst     import *
-from nucleardatapy.env     import *
-from nucleardatapy.param   import *
+from nucleardatapy.cst                      import *
+from nucleardatapy.env                      import *
+from nucleardatapy.param                    import *
 #
-from nucleardatapy.eos     import *
+from nucleardatapy.eos                      import *
 #
-from nucleardatapy.corr    import *
+from nucleardatapy.corr                     import *
 #
-from nucleardatapy.nuc     import *
+from nucleardatapy.nuc                      import *
+from nucleardatapy.nuc.setup_nskin_exp      import *
+from nucleardatapy.nuc.setup_nskin_theo     import *
 #
-from nucleardatapy.crust   import *
+from nucleardatapy.crust                    import *
 #
-from nucleardatapy.astro   import *
+from nucleardatapy.astro                    import *
 #
