d5ccd4502f3828890827c4937f6c9ac083843d46
J.Margueron <j.margueron@ipnl.in2p3.fr>
2025-01-17 11:14:58 -0500

    small modifications of the hnuc module

--- version1.0/nucleardatapy/hnuc/__init__.py
+++ version1.0/nucleardatapy/hnuc/__init__.py
@@ -2,5 +2,5 @@
 This module provides microscopic, phenomenological and experimental data constraints.
 """
 #
-from nucleardatapy.hnuc.setup_spe_exp     import *
+from nucleardatapy.hnuc.setup_be1L_exp     import *
 #
