d7b5ccf89f595d17707f8c4ddc4e5315589315d3
J.Margueron <36551470+jeromemargueron@users.noreply.github.com>
2025-03-17 14:28:15 -0400

    with BHF effective masses and empirical relation

--- version-0.2/nucleardatapy/matter/setup_micro_effmass.py
+++ version-0.2/nucleardatapy/matter/setup_micro_effmass.py
@@ -8,6 +8,15 @@ import random
 
 import nucleardatapy as nuda
 
+def effmass_emp( den , delta, mb = 'BHF' ):
+    if mb == 'BHF':
+        kfsat = 1.32
+        kfn = nuda.kf_n( nuda.cst.half * ( 1.0 + delta ) * den )
+        kfp = nuda.kf_n( nuda.cst.half * ( 1.0 - delta ) * den )
+        ms_n = 1.0 / ( 1.0 + 0.20 * (kfp/kfsat)**3.5 )
+        ms_p = 1.0 / ( 1.0 + 0.20 * (kfn/kfsat)**3.5 )
+    return ms_n, ms_p
+
 def micro_effmass_models( matter = 'NM' ):
     """
     Return a list with the name of the models available in this toolkit and \
@@ -24,7 +33,7 @@ def micro_effmass_models( matter = 'NM' ):
     #
     if nuda.env.verb: print("\nEnter micro_effmass_models()")
     # '2008-AFDMC-NM', 
-    models_all = [ '2008-BCS-NM', \
+    models_all = [ '2006-BHF-SM-Av18', '2006-BHF-AM-Av18', '2008-BCS-NM', \
             '2017-MBPT-NM-GAP-EMG-450-500-N2LO', '2017-MBPT-NM-GAP-EMG-450-500-N3LO', '2017-MBPT-NM-GAP-EMG-450-700-N2LO', \
             '2017-MBPT-NM-GAP-EMG-450-700-N3LO', '2017-MBPT-NM-GAP-EM-500-N2LO', '2017-MBPT-NM-GAP-EM-500-N3LO', \
             '2022-AFDMC-NM' ]
@@ -85,7 +94,51 @@ class setupMicroEffmass():
             print('setup_micro_effmass: -- Exit the code --')
             exit()
         #
-        if model.lower() == '2008-bcs-nm':
+        if model.lower() == '2006-bhf-sm-av18':
+            #
+            file_in = os.path.join(nuda.param.path_data,'matter/micro/2006-BHF/2006-BHF-Av18-effmass-SM.dat')
+            if nuda.env.verb: print('Reads file:',file_in)
+            self.ref = 'L.G. Cao, U. Lombardo, C.W. Shen, N.V. Giai, Phys. Rev. C 73, 014313 (2006)'
+            self.note = ""
+            self.label = 'BHF-2006'
+            self.marker = 'o'
+            self.every = 1
+            #self.linestyle = 'dotted'
+            self.err = False
+            self.nm_effmass_err = None
+            self.sm_den, self.sm_effmass \
+                = np.loadtxt( file_in, usecols=(0,1), unpack = True )
+            self.sm_kfn = nuda.kf_n( nuda.cst.half * self.sm_den )
+            #
+        elif model.lower() == '2006-bhf-am-av18':
+            #
+            file_in_00 = os.path.join(nuda.param.path_data,'matter/micro/2006-BHF/2006-BHF-Av18-effmass-SM.dat')
+            file_in_02 = os.path.join(nuda.param.path_data,'matter/micro/2006-BHF/2006-BHF-Av18-effmass-beta0.2.dat')
+            file_in_04 = os.path.join(nuda.param.path_data,'matter/micro/2006-BHF/2006-BHF-Av18-effmass-beta0.4.dat')
+            self.ref = 'L.G. Cao, U. Lombardo, C.W. Shen, N.V. Giai, Phys. Rev. C 73, 014313 (2006)'
+            self.note = ""
+            self.label = 'BHF-2006'
+            self.marker = 'o'
+            self.every = 1
+            #self.linestyle = 'dotted'
+            self.err = False
+            self.nm_effmass_err = None
+            self.sm_den, self.sm_effmass_p \
+                = np.loadtxt( file_in_00, usecols=(0,1), unpack = True )
+            self.sm_effmass_n = self.sm_effmass_p
+            self.sm_kfn = nuda.kf_n( nuda.cst.half * ( 1.0 + 0.0 ) * self.sm_den )
+            #
+            # asymmetric matter with delta = 0.2
+            self.am02_den, self.am02_effmass_p, self.am02_effmass_n \
+                = np.loadtxt( file_in_02, usecols=(0,1,2), unpack = True )
+            self.am02_kfn = nuda.kf_n( nuda.cst.half * ( 1.0 + 0.2 ) * self.am02_den )
+            #
+            # asymmetric matter with delta = 0.4
+            self.am04_den, self.am04_effmass_p, self.am04_effmass_n \
+                = np.loadtxt( file_in_04, usecols=(0,1,2), unpack = True )
+            self.am04_kfn = nuda.kf_n( nuda.cst.half * ( 1.0 + 0.4 ) * self.am04_den )
+            #
+        elif model.lower() == '2008-bcs-nm':
             #
             file_in = os.path.join(nuda.param.path_data,'matter/micro/2008-BCS-NM.dat')
             if nuda.env.verb: print('Reads file:',file_in)
--- version-0.2/nucleardatapy_samples/matter_setupMicro_effmass_plot.py
+++ version-0.2/nucleardatapy_samples/matter_setupMicro_effmass_plot.py
@@ -15,15 +15,29 @@ def main():
     # Neutron Matter (NM)
     # ===============================
     #
-    # list the available models in NM
+    # plot effective mass in NM
     #
     models, models_lower, models_all, models_all_lower = nuda.matter.micro_effmass_models( matter = 'NM' )
-    #
-    # plot effective mass
     #
     pname = 'figs/plot_matter_setupMicro_effmass_NM.png'
     #
     nuda.fig.matter_setupMicro_effmass_fig( pname, models, matter = 'NM' )
+    #
+    # plot effective mass in SM
+    #
+    models, models_lower, models_all, models_all_lower = nuda.matter.micro_effmass_models( matter = 'SM' )
+    #
+    pname = 'figs/plot_matter_setupMicro_effmass_SM.png'
+    #
+    nuda.fig.matter_setupMicro_effmass_fig( pname, models, matter = 'SM' )
+    #
+    # plot effective mass in AM
+    #
+    models, models_lower, models_all, models_all_lower = nuda.matter.micro_effmass_models( matter = 'AM' )
+    #
+    pname = 'figs/plot_matter_setupMicro_effmass_AM.png'
+    #
+    nuda.fig.matter_setupMicro_effmass_fig( pname, models, matter = 'AM' )
     #
     print(50*'-')
     print("Exit matter_setupMicro_effmass_plot.py:")
